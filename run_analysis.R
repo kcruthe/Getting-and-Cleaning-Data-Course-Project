e8dd245959edbf8fe630eb06b1ffdf6baf0f0ae4
kcruthe <kcruthe@gmail.com>
2016-08-24 19:08:08 +0700

    run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -17,41 +17,35 @@ library(tidyr)
 
 # Load the files that describe the features and observations
 x_label <- read.table("~/Work/R/data/UCI HAR Dataset/features.txt")
+mean_std <- grep(("-mean|-std"), x_label[,2])                        # Indices of x_label which contain mean & standard deviation
+x_label <- as.character(x_label[mean_std,2])
 activity_label <- read.table("~/Work/R/data/UCI HAR Dataset/activity_labels.txt")
-subject_train <- read.table("~/Work/R/data/UCI HAR Dataset/train/subject_train.txt")
-subject_test <- read.table("~/Work/R/data/UCI HAR Dataset/test/subject_test.txt")
-subject <- rbind(subject_train,subject_test)
-colnames(subject) <- "Subject"
 
-# Load the training and test "x" data and merge them
+# Assemble the training data
 x_train <- read.table("~/Work/R/data/UCI HAR Dataset/train/X_train.txt")
-x_test <- read.table("~/Work/R/data/UCI HAR Dataset/test/X_test.txt")
-x_data <- rbind(x_train, x_test)
-
-# Label the data set with descriptive variable names
-colnames(x_data) <- x_label[,2]              
-
-# Extract the mean and standard deviation for each measurement
-mean_Freq <- c(grep("meanFreq", x_label[,2]))           # Indices of x_label which contain meanFreq
-x_data <- x_data[,-mean_Freq]                           # Remove meanFreq from x_data
-x_label <- x_label[-mean_Freq,]                         # Remove meanFreq from x_label
-colnames(x_data) <- x_label[,2]                         # Update x_data column names
-mean_std <- c(grep(("-mean|-std"), x_label[,2]))        # Indices of x_label which contain remaining mean & standard deviation
-x_data <- x_data[,mean_std]                             # Subset x_label by mean_std
-x_data <- cbind(x_data,subject)                         # Attach "Subject" data
-
-# Load the training and test "y" data and merge them
+x_train <- x_train[,mean_std]                                           # Extract the mean and standard deviation for each measurement
 y_train <- read.table("~/Work/R/data/UCI HAR Dataset/train/Y_train.txt")
-y_test <- read.table("~/Work/R/data/UCI HAR Dataset/test/Y_test.txt")
-y_data <- rbind(y_train, y_test)
+subject_train <- read.table("~/Work/R/data/UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(x_train, y_train, subject_train)
 
-# Make the activity names descriptive
-y_data <- arrange(join(y_data,activity_label),V1)
-colnames(y_data) <- c("Activity_ID", "Activity")
+# Assemble the test data
+x_test <- read.table("~/Work/R/data/UCI HAR Dataset/test/X_test.txt")
+x_test <- x_test[,mean_std]                                             # Extract the mean and standard deviation for each measurement
+y_test <- read.table("~/Work/R/data/UCI HAR Dataset/test/Y_test.txt")
+subject_test <- read.table("~/Work/R/data/UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(x_test, y_test, subject_test)
 
 # Merge the data tables
-data <- cbind(x_data, y_data)
+data <- rbind(train, test)
+
+# Label the data set with descriptive variable names
+colnames(data) <- c(x_label, "Activity", "Subject")              
 
 # Create an independent, tidy data set with the average of each variable for each activity and each subject
-tidy_data <- data %>% group_by(Activity_ID, Activity, Subject) %>% summarise_each(funs(mean))
 
+# Make Subject and Activity into factors for summarization
+data$Subject <- as.factor(data$Subject)
+data$Activity <- factor(data$Activity, levels = activity_label[,1], labels = activity_label[,2])
+
+# Summarize the data set
+tidy_data <- data %>% group_by(Subject, Activity) %>% summarise_each(funs(mean))
